643878df5cefa7fc00da5fbf5435f9c975131764
Yann Simon <yann.simon.fr@gmail.com>
2014-03-20 09:33:59 +0100

    ignore onAllDataRead and use input.isFinished() as completion signal

--- src/main/java/com/yann/ReadListenerImpl.java
+++ src/main/java/com/yann/ReadListenerImpl.java
@@ -22,37 +22,42 @@ public class ReadListenerImpl implements ReadListener {
 
     @Override
     public void onDataAvailable() throws IOException {
-        synchronized (input) {
-            System.out.println(threadId() + "ReadListenerImpl: onDataAvailable");
-            (new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    synchronized (input) {
-                        int len = -1;
-                        int localRead = 0;
-                        byte b[] = new byte[1024];
-                        try {
-                            while (input.isReady() && (len = input.read(b)) != -1) {
-                                localRead += len;
-                            }
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                        try {
-                            // simulate wait to check back pressure
-                            Thread.sleep((int) (Math.random() * 200));
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-
-                        read += localRead;
-                        System.out.println(threadId() + "read till now: " + read);
-                    }
+        System.out.println(threadId() + "ReadListenerImpl: onDataAvailable");
+        (new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // simulate wait for database to be ready
+                    Thread.sleep((int) (Math.random() * 200));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            })).start();
-            System.out.println(threadId() + "onDataAvailable end");
-        }
+                int len = -1;
+                int localRead = 0;
+                byte b[] = new byte[1024];
+                try {
+                    while (input.isReady() && (len = input.read(b)) != -1) {
+                        localRead += len;
+                    }
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    // simulate wait for intensive take for read chunk
+                    Thread.sleep((int) (Math.random() * 200));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                read += localRead;
+                System.out.println(threadId() + "read till now: " + read);
+                if (input.isFinished()) {
+                    asyncContext.complete();
+                }
+            }
+        })).start();
+        System.out.println(threadId() + "onDataAvailable end");
     }
 
     public int getRead() {
@@ -61,10 +66,7 @@ public class ReadListenerImpl implements ReadListener {
 
     @Override
     public void onAllDataRead() throws IOException {
-        synchronized (input) {
-            System.out.println(threadId() + "onAllDataRead");
-            asyncContext.complete();
-        }
+        System.out.println(threadId() + "onAllDataRead");
     }
 
     @Override
